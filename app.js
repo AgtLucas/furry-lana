02e533d3b77f1d404f98068fd0b78da33c957df7
Lucas <lucas@agtlucas.com>
2015-02-04 21:48:11 -0200

    Update

--- app.js
+++ app.js
@@ -19,3 +19,30 @@ function fb(n) {
 
   return result;
 }
+
+/*
+# 6
+2 < 6
+next = 1
+first = 1;
+second = 1
+[0, 1, 1]
+
+3 < 6
+next = 2
+first = 1
+second = 2
+[0, 1, 1, 2]
+
+4 < 6
+next = 3
+first = 2
+second = 3
+[0, 1, 1, 2, 3]
+
+5 < 6
+next = 5
+first = 3
+second = 5
+[0, 1, 1, 2, 3, 5]
+*/
