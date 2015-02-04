ee1a4221f0e304e09953b7ecb96d99564fcb696a
Lucas <lucas@agtlucas.com>
2015-02-04 21:43:29 -0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 'use strict';
 
 function fb(n) {
-  if (n <= 1) {
-    return n;
-  } else {
-    return fb(n-1) + fb(n-2);
+  var first = 0,
+    second = 1,
+    next,
+    count = 2,
+    result = [first, second];
+
+  if (n < 2)
+    return "No way, Jose!";
+
+  while (count++ < n) {
+    next = first + second;
+    first = second;
+    second = next;
+    result.push(next);
   }
+
+  return result;
 }
